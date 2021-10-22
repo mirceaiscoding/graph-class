a2c997693a3451e92ba13b0d9620e046c22a7104
Bina Mircea <bina.mircea2001@gmail.com>
2021-10-22 17:00:06 +0300

    implemented biconnected components

--- graph.cpp
+++ graph.cpp
@@ -1,12 +1,14 @@
 #include <vector>
 #include <queue>
+#include <stack>
 #include <fstream>
 #include <iostream>
 using namespace std;
-ifstream fin("dfs.in");
-ofstream fout("dfs.out");
+ifstream fin("biconex.in");
+ofstream fout("biconex.out");
 
 #define NO_PATH -1
+#define NO_PARENT_NODE -1
 
 /**
  * @brief Graph class that implements algorithms
@@ -39,6 +41,58 @@ private:
             if (!isVisited[targetNode])
             {
                 DFS(targetNode, isVisited);
+            }
+        }
+    }
+
+    /**
+     * @brief Get a list of biconnected components (lists of nodes) in `biconnectedComponents`
+     * 
+     * @param node current node
+     * @param currentDepth current depth
+     * @param parentNode parent node of current node
+     * @param biconnectedComponents the biconnected components will be stored here
+     * @param depth depth of nodes (distance from root)
+     * @param low minimum level a node can reach (without going back through parent nodes)
+     * @param isVisited if a node is visited or not
+     * @param visitedNodes order in which nodes are visited in the DFS
+     */
+    void torjanAlgorithm(int node, int currentDepth, int parentNode, vector<vector<int> > &biconnectedComponents,
+                         int depth[], int low[], bool isVisited[], stack<int> &visitedNodes)
+    {
+        isVisited[node] = true;
+        depth[node] = currentDepth;
+        low[node] = currentDepth;
+        visitedNodes.push(node);
+
+        for (int i = 0; i < edges[node].size(); i++)
+        {
+            int targetNode = edges[node][i];
+            if (targetNode != parentNode)
+            {
+                if (isVisited[targetNode])
+                {
+                    low[node] = min(low[node], depth[targetNode]);
+                }
+                else
+                {
+                    torjanAlgorithm(targetNode, currentDepth + 1, node, biconnectedComponents, depth, low, isVisited, visitedNodes);
+                    low[node] = min(low[node], low[targetNode]);
+
+                    if (low[targetNode] >= depth[node])
+                    {
+                        vector<int> biconnectedComponent;
+                        int currentNode;
+                        do
+                        {
+                            currentNode = visitedNodes.top();
+                            biconnectedComponent.push_back(currentNode);
+                            visitedNodes.pop();
+                        } while (currentNode != targetNode);
+                        biconnectedComponent.push_back(node);
+                        biconnectedComponents.push_back(biconnectedComponent);
+                    }
+                }
             }
         }
     }
@@ -148,7 +202,7 @@ public:
             isVisited[i] = false;
         }
         int numberOfConexComponents = 0;
-        
+
         for (int node = 0; node < numberOfNodes; node++)
         {
             if (!isVisited[node])
@@ -158,6 +212,24 @@ public:
             }
         }
         return numberOfConexComponents;
+    }
+
+    vector<vector<int> > getBiconnectedComponents(int startNode)
+    {
+        vector<vector<int> > biconnectedComponents;
+        int depth[numberOfNodes];
+        int low[numberOfNodes];
+        bool isVisited[numberOfNodes];
+        stack<int> visitedNodes;
+        for (int node = 0; node < numberOfNodes; node++)
+        {
+            isVisited[node] = false;
+        }
+
+        // Call algorithm with startNode as root
+        torjanAlgorithm(startNode, 0, NO_PARENT_NODE, biconnectedComponents, depth, low, isVisited, visitedNodes);
+
+        return biconnectedComponents;
     }
 };
 
@@ -169,5 +241,16 @@ int main()
     Graph graph(numberOfNodes, false);
     graph.readEdges(fin, numberOfEdges, false);
 
-    fout << graph.getNumberOfConexComponents();
+    vector<vector<int> > biconnectedComponents = graph.getBiconnectedComponents(0);
+    fout << biconnectedComponents.size() << "\n";
+    for (int i = 0; i < biconnectedComponents.size(); i++)
+    {
+        vector<int> component = biconnectedComponents[i];
+        for (int j = 0; j < component.size(); j++)
+        {
+            int node = component[j] + 1;
+            fout << node << " ";
+        }
+        fout << "\n";
+    }
 }
