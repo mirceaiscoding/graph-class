2dd2ff9037698dd5e632019f59af9099590b3e79
Bina Mircea <bina.mircea2001@gmail.com>
2021-10-24 12:57:56 +0300

    Implemented strongly connected components

--- graph.cpp
+++ graph.cpp
@@ -4,8 +4,8 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
-ifstream fin("bfs.in");
-ofstream fout("bfs.out");
+ifstream fin("ctc.in");
+ofstream fout("ctc.out");
 
 #define NO_PATH -1
 #define NO_PARENT_NODE -1
@@ -58,7 +58,7 @@ private:
      * @param visitedNodes order in which nodes are visited in the DFS
      */
     void findBiconnectedComponents(int node, int currentDepth, int parentNode, vector<vector<int> > &biconnectedComponents,
-                         int depth[], int low[], bool isVisited[], stack<int> &visitedNodes)
+                                   int depth[], int low[], bool isVisited[], stack<int> &visitedNodes)
     {
         isVisited[node] = true;
         depth[node] = currentDepth;
@@ -94,6 +94,57 @@ private:
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * @brief Get a list of strongly connected components (lists of nodes) in `stronglyConnectedComponents`
+     * 
+     * @param node current node
+     * @param index smallest unused index
+     * @param parentNode parent node of current node
+     * @param stronglyConnectedComponents the strongly connected components will be stored here
+     * @param depth depth of nodes (distance from root)
+     * @param low minimum level a node can reach (without going back through parent nodes)
+     * @param isVisited if a node is visited or not
+     * @param inStack if the node is in the visitedNodes stack
+     * @param visitedNodes order in which nodes are visited in the DFS
+     */
+    void findStronglyConnectedComponents(int node, int &index, int parentNode, vector<vector<int> > &stronglyConnectedComponents,
+                                         int depth[], int low[], bool isVisited[], bool inStack[], stack<int> &visitedNodes)
+    {
+        isVisited[node] = true;
+        depth[node] = index;
+        low[node] = index;
+        index++;
+        visitedNodes.push(node);
+        inStack[node] = true;
+        for (int i = 0; i < edges[node].size(); i++)
+        {
+            int targetNode = edges[node][i];
+            if (!isVisited[targetNode])
+            {
+                findStronglyConnectedComponents(targetNode, index, node, stronglyConnectedComponents, depth, low, isVisited, inStack, visitedNodes);
+                low[node] = min(low[node], low[targetNode]);
+            }
+            else if (inStack[targetNode])
+            {
+                low[node] = min(low[node], low[targetNode]);
+            }
+        }
+
+        if (low[node] == depth[node])
+        {
+            vector<int> stronglyConnectedComponent;
+            int currentNode;
+            do
+            {
+                currentNode = visitedNodes.top();
+                stronglyConnectedComponent.push_back(currentNode);
+                visitedNodes.pop();
+                inStack[currentNode] = false;
+            } while (currentNode != node);
+            stronglyConnectedComponents.push_back(stronglyConnectedComponent);
         }
     }
 
@@ -239,20 +290,59 @@ public:
 
         return biconnectedComponents;
     }
+
+    /**
+     * @brief Get the strongly connected components of the Graph
+     * 
+     * @return vector<vector<int> > vector of strongly connected components (list of nodes)
+     */
+    vector<vector<int> > getStronglyConnectedComponents()
+    {
+        vector<vector<int> > stronglyConnectedComponents;
+        int depth[numberOfNodes];
+        int low[numberOfNodes];
+        bool isVisited[numberOfNodes];
+        bool inStack[numberOfNodes];
+        stack<int> visitedNodes;
+        int index = 0;
+        for (int node = 0; node < numberOfNodes; node++)
+        {
+            isVisited[node] = false;
+            inStack[node] = false;
+        }
+
+        for (int node = 0; node < numberOfNodes; node++)
+        {
+            if (!isVisited[node])
+            {
+                // Call recursive function with node as root
+                findStronglyConnectedComponents(node, index, NO_PARENT_NODE, stronglyConnectedComponents, depth, low, isVisited, inStack, visitedNodes);
+            }
+        }
+
+        return stronglyConnectedComponents;
+    }
 };
 
 int main()
 {
-    int numberOfNodes, numberOfEdges, startNode;
-    fin >> numberOfNodes >> numberOfEdges >> startNode;
-    startNode--; // make it zero-based
- 
+    int numberOfNodes, numberOfEdges;
+    fin >> numberOfNodes >> numberOfEdges;
+
     Graph graph(numberOfNodes, true);
     graph.readEdges(fin, numberOfEdges, false);
 
-    vector<int> distances = graph.getMinimumDistances(startNode);
-    for (int i = 0; i < numberOfNodes; i++)
+    vector<vector<int> > stronglyConnectedComponents = graph.getStronglyConnectedComponents();
+
+    fout << stronglyConnectedComponents.size() << "\n";
+    for (int i = 0; i < stronglyConnectedComponents.size(); i++)
     {
-        fout << distances[i] << " ";
+        vector<int> component = stronglyConnectedComponents[i];
+        for (int j = 0; j < component.size(); j++)
+        {
+            int node = component[j] + 1;
+            fout << node << " ";
+        }
+        fout << "\n";
     }
 }
